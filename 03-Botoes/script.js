5d8d867f94f2123c07517fac12a7d77d005f5387
Guilhermegfg <guilhermegfg2011@hotmail.com>
2023-07-14 04:26:28 -0300

    updateJavaScriptBotoes

---
+++ 03-Botoes/script.js
@@ -0,0 +1,49 @@
+function toggleDefault() {
+
+    var button = document.getElementById("principal")
+    button.className = "";
+
+    document.documentElement.classList.toggle("default")
+}
+
+function toggleHover() {
+
+    var button = document.getElementById("principal")
+    button.className = "";
+
+    document.documentElement.classList.toggle("hover")
+}
+
+function toggleFocus() {
+
+    var button = document.getElementById("principal")
+    button.className = "";
+
+    document.documentElement.classList.toggle("focus")
+}
+
+function toggleDisabled() {
+
+    var button = document.getElementById("principal")
+    button.className = "";
+
+    document.documentElement.classList.toggle("disabled")
+}
+
+function toggleLoading() {
+
+    var button = document.getElementById("principal")
+    button.className = "";
+
+    document.documentElement.classList.toggle("loading")
+
+}
+
+function toggleMovable() {
+    
+    var button = document.getElementById("principal")
+    button.className = "";
+
+    document.documentElement.classList.toggle("movable")
+}
+
