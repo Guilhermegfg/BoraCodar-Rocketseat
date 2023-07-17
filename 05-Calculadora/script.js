7aa40148ef70d87d148bfa514664e4ceab3a1b5c
Guilhermegfg <guilhermegfg2011@hotmail.com>
2023-07-17 05:56:21 -0300

    updateScript

---
+++ 05-Calculadora/script.js
@@ -0,0 +1,37 @@
+var display = document.getElementById('display');
+
+function adicionarAoDisplay (value) {
+    display.value += value;
+}
+
+function adicionarSinalAoDisplay(value) {
+    display.value += ` ${value} `
+}
+
+function PositivoNegativo () {
+    display.value += "-";
+}
+
+function limpar () {
+    display.value = "";
+}
+
+function calcular() {
+
+    if (display.value === "") {
+
+        display.value = "";
+
+    } else{
+        try {
+            const resultado = eval(display.value);
+            display.value = resultado;
+        } catch (error) {
+            display.value = "erro"
+        }
+        
+    }
+
+
+   
+}
